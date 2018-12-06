aab7b29bbd4c9c26feb9e39b23ee5db926195396
Joao Portela <joao.portela@gmail.com>
2018-12-06 22:56:40 +0000

    adds first example (start)

---
+++ start/renderer.js
@@ -0,0 +1,6 @@
+const rootElement = document.getElementById('root');
+
+const helloElement = document.createElement('h1');
+helloElement.textContent = 'Hello, World!'
+
+rootElement.appendChild(helloElement);
